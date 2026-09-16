dbe4cb1eede6a5445a8cc410f9282c25540a77b5
Alyssah Van Orman <285124057+geniusinthemaking-arch@users.noreply.github.com>
2026-09-16 13:32:51 -0500

    Added initial css

---
+++ index.js
@@ -0,0 +1,21 @@
+// API : https://ghibliapi.vercel.app/films      22 films
+
+async function main () {
+    const films = await fetch("https://ghibliapi.vercel.app/films");
+    const filmsData = await films.json();
+    const filmListEl = document.querySelector('.film-list');
+    filmListEl.innerHTML = filmsData.map((film) => filmsHTML(film)).join("");
+};
+
+main();
+
+function filmsHTML(film) {
+    return `<div class="film-card">
+        <div class="film-card__container">
+            <h3>${film.title}</h3>
+            <p><b>Original Title:</b> ${film.original_title}</p>
+            <p><b>Description:</b> ${film.description}</p>
+            <p><b>Release Year:</b> ${film.release_date}</p>
+        </div>
+    </div>`
+};
